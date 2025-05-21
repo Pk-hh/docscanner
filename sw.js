e2ec0c55054bd7317249cadd14f5a52636e1791b
Pk-hh <pradeep4045t7@gmail.com>
2025-05-21 19:07:53 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,36 @@
 const CACHE_NAME = 'docscanner-cache-v1';
 const urlsToCache = [
-  '.',
-  './index.html',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  // Add other resources you want cached
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  'https://docs.opencv.org/4.x/opencv.js'
 ];
 
-self.addEventListener('install', event => {
+// Install SW and cache files
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', event => {
+// Activate SW and remove old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      })
+    ))
+  );
+});
+
+// Fetch cached files first, fallback to network
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
   );
 });
